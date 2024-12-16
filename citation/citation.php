4c86c37ec42560b82fe3a464d03db206ab5a647d
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2024-12-16 23:53:21 +0100

    update

---
+++ citation/citation.php
@@ -0,0 +1,11 @@
+<?php
+$citations = [
+"Si tu sais que tu ne sais pas, tu sauras, mais si tu ne sais pas que tu ne sais pas, tu ne sauras jamais. - Saidou Abatcha",
+"Le plus grand risque est de ne prendre aucun risque. - Mark Zuckerberg",
+"La connaissance s'acquiert par l'expérience, tout le reste n'est que de l'information. - Albert Einstein",
+" Écrire est un acte d’amour. S’il ne l’est pas, il n’est qu’écriture. - Jean Cocteau",
+"L'échec est une opportunité de recommencer plus intelligemment. - Henry Ford",
+"La persévérance est la clé du succès. - Mazouz Hacène",
+"L’amour est comme le vent, on ne peut pas le voir mais on peut le sentir. – Nicholas Sparks.",
+"On peut apprendre à un ordinateur à dire : “Je t’aime”, mais on ne peut pas lui apprendre à aimer. - Albert Jacquard"
+];
---
+++ citation/index.php
@@ -0,0 +1,30 @@
+<?php
+    include 'citation.php';
+    if(isset($citations)&& !empty($citations)){
+        $randomize= array_rand($citations);
+        $citarand=$citations[$randomize];
+    }else{
+        $randomize= array_rand($citations);
+        $citarand=$citations[$randomize];
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Citations</title>
+</head>
+<body>
+    <section>
+        <h1>Tsif citation:</h1>
+        <div class="blue">
+            <i><?= $citarand?></i>
+        </div>
+        <div>
+            <a href="index.php">Generer</a>
+        </div>
+    </section>
+</body>
+</html>
