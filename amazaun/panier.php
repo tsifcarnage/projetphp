a6edc55b877493e0699fb7e3ef6ff6ae8b6f730d
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2024-12-19 18:00:45 +0100

    Update panier.php

--- amazaun/panier.php
+++ amazaun/panier.php
@@ -1,10 +1,10 @@
 <?php               
-if (array_sum($quantite) > 0) {//arraysum permet de calculer la total du quantite,verifier si cest plus grand que zero les quantites
+if (array_sum($quantite) > 0) {//Arraysum permet de calculer la total du quantite,verifier si cest plus grand que zero les quantites
     $total = 0; 
     foreach ($articles as $article) {
         $nom = $article["nom"];
         $prix = $article["prix"];
-        $quantite_article = $quantite[$nom]; 
+        $quantite_article = $quantite[$nom]; //Prend la quantite de chaque nom
         if ($quantite_article > 0) {                           
             $total += $prix * $quantite_article; // Calcule le total
             echo '<ul>';
