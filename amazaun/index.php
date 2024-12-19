dd711bfbd0b4c0856b0551579c1762496b97e79e
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2024-12-19 13:10:13 +0100

    update

--- amazaun/index.php
+++ amazaun/index.php
@@ -1,6 +1,10 @@
 <?php 
     require 'formulaire.php';
     require 'articles.php';
+    $nikeq=isset($_POST['quantite_Nike_Air_AF1']) && !empty($_POST['quantite_Nike_Air_AF1'])?$_POST['quantite_Nike_Air_AF1']:'';
+    $shirtq=isset($_POST['quantite_T-shirt']) && !empty($_POST['quantite_T-shirt'])?$_POST['quantite_T-shirt']:'';
+    $pantq=isset($_POST['quantite_Pantalon']) && !empty($_POST['quantite_Pantalon'])?$_POST['quantite_Pantalon']:'';
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,15 +14,22 @@
     <link rel="stylesheet" href="style.css">
     <title>Amazaun</title>
 </head>
+
 <body>
     <div class="bloc2">
         <div>
             <?=getFormulaire($articles)?>
         </div>
         <div>
-            <form action="" method="POST">
-                <h1>Votre Panier:</h1>
-            </form>
+            <h1>Votre Panier:</h1>
+            <?php
+                $resultat=0;
+                if($nikeq>0 &&$shirtq>0 && $pantq>0 ){
+    
+                }
+            ?>
+            <h4>Montant de la commande:0€</h4>
+            
         </div>
         
     </div>
